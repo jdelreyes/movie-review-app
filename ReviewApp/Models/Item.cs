8bf2226476267e58cddeb2e71c702177b0500448
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2024-05-20 02:06:12 -0400

    establish relationships between tables

--- ReviewApp/Models/Item.cs
+++ ReviewApp/Models/Item.cs
@@ -5,4 +5,5 @@ public class Item
     public int Id { get; set; }
     public string Name { get; set; }
     public DateTime DateCreated { get; set; }
+    public ICollection<Review> Reviews { get; set; }
 }
--- ReviewApp/Models/Owner.cs
+++ ReviewApp/Models/Owner.cs
@@ -4,4 +4,5 @@ public class Owner
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public Country Country { get; set; }
 }
